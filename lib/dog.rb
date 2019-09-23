9cd2b778a08f5afca5655db9057ab2540108e0cb
Alexander Bispo <alexbispo173@gmail.com>
2019-09-23 01:04:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,9 @@ class Dog
     @name = name
   end
  
-  
+  def name
+    @name
+  end
  
   def name=(new_name)
     @name = new_name
@@ -14,7 +16,9 @@ class Dog
     @breed = breed
   end
  
-  
+  def breed
+    @breed
+  end
  
   def breed=(new_breed)
     @breed = new_breed
